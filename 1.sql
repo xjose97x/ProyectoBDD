795cb4002591c6717d84506ae5888547d8b87034
Miguel <miguelbritosuq@outlook.es>
2022-06-28 14:59:32 -0500

    Ajuste sinonimos, permisos

--- 1.sql
+++ 1.sql
@@ -1,18 +1,35 @@
 USE master
 GO
 
-CREATE LOGIN administrador WITH PASSWORD = N'flicks4uEcuador2022'
-GO
+-- **** CREACIÓN DE LOGINS ****
+-- Creación login administrador
+IF NOT EXISTS(SELECT name FROM sys.syslogins
+			  WHERE NAME = 'administrador')
+	CREATE LOGIN administrador WITH PASSWORD = N'flicks4uEcuador2022'
 
-CREATE LOGIN gestorFunciones WITH PASSWORD = N'salasCineFlicks4uEcuador'
-GO
+-- Creación login gestorFunciones
+IF NOT EXISTS(SELECT name FROM sys.syslogins
+			  WHERE NAME = 'gestorFunciones')
+	CREATE LOGIN gestorFunciones WITH PASSWORD = N'salasCineFlicks4uEcuador'
 
-CREATE LOGIN recursosHumanos WITH PASSWORD = N'recursosHumanosFlicks4UEcuador'
-GO
+-- Creación login recursosHumanos
+IF NOT EXISTS(SELECT name FROM sys.syslogins
+			  WHERE NAME = 'recursosHumanos')
+	CREATE LOGIN recursosHumanos WITH PASSWORD = N'recursosHumanosFlicks4UEcuador'
 
-CREATE LOGIN visualizadorReportes WITH PASSWORD = N'visulizadorFlicks4UEcuador'
-GO
+-- Creación login visualizadorReportes
+IF NOT EXISTS(SELECT name FROM sys.syslogins
+			  WHERE NAME = 'visualizadorReportes')
+	CREATE LOGIN visualizadorReportes WITH PASSWORD = N'visulizadorFlicks4UEcuador'
 
+
+-- **** CREACIÓN DE BASE DE DATOS ****
+IF(DB_ID('Flicks4U') IS NOT NULL)
+BEGIN
+	alter database Flicks4U set single_user with rollback immediate
+	DROP DATABASE Flicks4U
+END
+GO
 
 CREATE DATABASE Flicks4U
 GO
@@ -20,43 +37,72 @@ GO
 USE Flicks4U
 GO
 
---Tipo de datos--
-CREATE TYPE dbo.email
+-- **** CREACIÓN DE TIPOS DE DATOS Y REGLAS ****
+
+-- Creación de tipo de dato email
+IF (TYPE_ID('email') IS NOT NULL)
+    DROP TYPE email
+GO
+
+CREATE TYPE email
 FROM nvarchar(50) NOT NULL
 GO
 Create Rule ck_email
 as @email LIKE '%@%.%'
 GO
-sp_bindrule ck_email,'dbo.email'
+sp_bindrule ck_email,'email'
 GO
 
-CREATE TYPE dbo.phoneNumber
+--Creación de tipo de dato phoneNumber
+IF (TYPE_ID('phoneNumber') IS NOT NULL)
+    DROP TYPE phoneNumber
+GO
+
+CREATE TYPE phoneNumber
 FROM nvarchar(50) NOT NULL
 GO
 Create Rule ck_phoneNumber
 as @phoneNumber LIKE '+%[0-9]% %[0-9]%'
 GO
-sp_bindrule ck_phoneNumber,'dbo.phoneNumber'
+sp_bindrule ck_phoneNumber,'phoneNumber'
 GO
 
-CREATE TYPE dbo.gender
+--Creación de tipo de dato gender
+IF (TYPE_ID('gender') IS NOT NULL)
+    DROP TYPE phoneNumber
+GO
+IF (TYPE_ID('gender') IS NOT NULL)
+    DROP TYPE phoneNumber
+GO
+
+CREATE TYPE gender
 FROM char(1) NOT NULL
 GO
 Create Rule ck_gender
 as @gender IN ('M', 'F', 'O')
 GO
-sp_bindrule ck_gender,'dbo.gender'
+sp_bindrule ck_gender,'gender'
 GO
 
-CREATE SCHEMA RecursosHumanos
+--Creación de Esquema RecursosHumanos
+IF NOT EXISTS (SELECT 1 FROM sys.schemas WHERE name = 'RecursosHumanos')
+    EXEC( 'CREATE SCHEMA RecursosHumanos' );
 GO
 
-CREATE SCHEMA Funciones
+--Creación de Esquema Funciones
+IF NOT EXISTS (SELECT 1 FROM sys.schemas WHERE name = 'Funciones')
+	EXEC( 'CREATE SCHEMA Funciones' );
 GO
 
-CREATE SCHEMA Reportes
+--Creación de Esquema Reportes
+IF NOT EXISTS (SELECT 1 FROM sys.schemas WHERE name = 'Reportes')
+	EXEC( 'CREATE SCHEMA Reportes' );
 GO
 
+-- **** CREACIÓN DE TABLAS ****
+-- Creación de tabla Empleado
+IF(OBJECT_ID('RecursosHumanos.Empleado') IS NOT NULL)
+	DROP TABLE RecursosHumanos.Empleado
 CREATE TABLE RecursosHumanos.Empleado
 (
 	Id SMALLINT IDENTITY (1, 1),
@@ -82,6 +128,9 @@ CREATE TABLE RecursosHumanos.Empleado
 )
 GO
 
+-- Creación de tabla HorarioLaboral
+IF(OBJECT_ID('RecursosHumanos.HorarioLaboral') IS NOT NULL)
+	DROP TABLE RecursosHumanos.HorarioLaboral
 CREATE TABLE RecursosHumanos.HorarioLaboral
 (
 	EmpleadoId SMALLINT NOT NULL,
@@ -95,6 +144,9 @@ CREATE TABLE RecursosHumanos.HorarioLaboral
 )
 GO
 
+-- Creación de tabla Pelicula
+IF(OBJECT_ID('Funciones.Pelicula') IS NOT NULL)
+	DROP TABLE Funciones.Pelicula
 CREATE TABLE Funciones.Pelicula
 (
 	Id INT IDENTITY (1,1),
@@ -113,6 +165,9 @@ CREATE TABLE Funciones.Pelicula
 )
 GO
 
+-- Creación de tabla Sala
+IF(OBJECT_ID('Funciones.Sala') IS NOT NULL)
+	DROP TABLE Funciones.Sala
 CREATE TABLE Funciones.Sala
 (
 	Id TINYINT IDENTITY (1,1),
@@ -125,6 +180,9 @@ CREATE TABLE Funciones.Sala
 )
 GO
 
+-- Creación de tabla Funcion
+IF(OBJECT_ID('Funciones.Funcion') IS NOT NULL)
+	DROP TABLE Funciones.Funcion
 CREATE TABLE Funciones.Funcion
 (
 	Id INT identity (1,1),
@@ -142,41 +200,102 @@ CREATE TABLE Funciones.Funcion
 )
 GO
 
+-- Creación de Sinónimo EmpleadoParaGestor para esquema de funciones.
+IF  EXISTS (SELECT * FROM sys.synonyms WHERE NAME = 'EmpleadoParaGestor')
+	DROP SYNONYM EmpleadoParaGestor
 CREATE SYNONYM EmpleadoParaGestor FOR RecursosHumanos.Empleado
-GO
-
-CREATE SYNONYM HorarioEmpleadoParaGestor FOR RecursosHumanos.HorarioLaboral
 GO
 
 ALTER SCHEMA Funciones TRANSFER OBJECT :: EmpleadoParaGestor
 GO
 
+-- Creación de Sinónimo HorarioEmpleadoParaGestor para esquema de funciones.
+IF  EXISTS (SELECT * FROM sys.synonyms WHERE NAME = 'HorarioEmpleadoParaGestor')
+	DROP SYNONYM HorarioEmpleadoParaGestor
+CREATE SYNONYM HorarioEmpleadoParaGestor FOR RecursosHumanos.HorarioLaboral
+GO
+
 ALTER SCHEMA Funciones TRANSFER OBJECT :: HorarioEmpleadoParaGestor
 GO
 
-
-CREATE USER [administrador] FOR LOGIN [administrador] WITH DEFAULT_SCHEMA=[dbo]
+-- Creación de Sinónimo PeliculaParaReportes para esquema de reportes.
+IF  EXISTS (SELECT * FROM sys.synonyms WHERE NAME = 'PeliculaParaReportes')
+	DROP SYNONYM PeliculaParaReportes
+CREATE SYNONYM PeliculaParaReportes FOR Funciones.Pelicula
 GO
 
-CREATE USER [gestorFunciones] FOR LOGIN [gestorFunciones] WITH DEFAULT_SCHEMA=[Funciones]
+ALTER SCHEMA Reportes TRANSFER OBJECT :: PeliculaParaReportes
 GO
 
-CREATE USER [RRHH] FOR LOGIN [recursosHumanos] WITH DEFAULT_SCHEMA=[RecursosHumanos]
+-- Creación de Sinónimo FuncionesParaReportes para esquema de reportes.
+IF  EXISTS (SELECT * FROM sys.synonyms WHERE NAME = 'FuncionesParaReportes')
+	DROP SYNONYM FuncionesParaReportes
+CREATE SYNONYM FuncionesParaReportes FOR Funciones.Funcion
 GO
 
-CREATE USER [visualizadorReportes] FOR LOGIN visualizadorReportes WITH DEFAULT_SCHEMA=[Reportes]
+ALTER SCHEMA Reportes TRANSFER OBJECT :: FuncionesParaReportes
 GO
 
+-- Creación de Sinónimo EmpleadoParaReportes para esquema de reportes.
+IF  EXISTS (SELECT * FROM sys.synonyms WHERE NAME = 'EmpleadoParaReportes')
+	DROP SYNONYM EmpleadoParaReportes
+CREATE SYNONYM EmpleadoParaReportes FOR RecursosHumanos.Empleado
+GO
+
+ALTER SCHEMA Reportes TRANSFER OBJECT :: EmpleadoParaReportes
+GO
+
+-- Creación de Sinónimo SalasParaReportes para esquema de reportes.
+IF  EXISTS (SELECT * FROM sys.synonyms WHERE NAME = 'SalasParaReportes')
+	DROP SYNONYM SalasParaReportes
+CREATE SYNONYM SalasParaReportes FOR Funciones.Sala
+GO
+
+ALTER SCHEMA Reportes TRANSFER OBJECT :: SalasParaReportes
+GO
+
+
+-- **** CREACIÓN DE USUARIOS ****
+-- Creación de usuario administrador
+IF DATABASE_PRINCIPAL_ID('administrador') IS NULL
+BEGIN
+    CREATE USER [administrador] FOR LOGIN [administrador] WITH DEFAULT_SCHEMA=[dbo]
+END
+GO
+
+-- Creación de usuario gestorFunciones
+IF DATABASE_PRINCIPAL_ID('gestorFunciones') IS NULL
+BEGIN
+	CREATE USER [gestorFunciones] FOR LOGIN [gestorFunciones] WITH DEFAULT_SCHEMA=[Funciones]
+END
+GO
+
+-- Creación de usuario RRHH
+IF DATABASE_PRINCIPAL_ID('RRHH') IS NULL
+BEGIN
+	CREATE USER [RRHH] FOR LOGIN [recursosHumanos] WITH DEFAULT_SCHEMA=[RecursosHumanos]
+END
+GO
+
+-- Creación de usuario visualizadorReportes
+IF DATABASE_PRINCIPAL_ID('visualizadorReportes') IS NULL
+BEGIN
+	CREATE USER [visualizadorReportes] FOR LOGIN visualizadorReportes WITH DEFAULT_SCHEMA=[Reportes]
+END
+GO
+
+-- **** ASIGNACIÓN DE PERMISOS ****
 -- Asignación de permiso a administrador como db_owner.
 EXEC sp_addrolemember N'db_owner', N'administrador'
 GO
 
--- Asignación de permiso a gestor de funciones de cine.
+-- Asignación de permisos a gestor de funciones de cine.
 GRANT SELECT, INSERT, DELETE, UPDATE
 ON SCHEMA :: Funciones
 TO gestorFunciones
 GO
 
+-- Asignación de permisos a RRHH
 DENY SELECT, INSERT, DELETE, UPDATE
 ON SCHEMA :: Funciones
 TO RRHH
@@ -187,6 +306,9 @@ DENY SELECT, INSERT, DELETE, UPDATE
 ON SCHEMA :: Funciones
 TO visualizadorReportes
 CASCADE
+
+-- Asignación de permisos a sinónimo EmpleadoParaGestor
+GRANT SELECT ON Funciones.EmpleadoParaGestor TO gestorFunciones
 GO
 
 -- Asignación de permiso a visualizador de reportes.
@@ -200,12 +322,10 @@ ON SCHEMA :: Reportes
 TO gestorFunciones
 GO
 
-
 DENY SELECT, INSERT, DELETE, UPDATE
 ON SCHEMA :: Reportes
 TO RRHH
 GO
-
 
 -- Asignación de permisos a gestor de recursos humanos.
 GRANT SELECT, INSERT, DELETE, UPDATE
@@ -220,25 +340,90 @@ GO
 
 DENY SELECT, INSERT, DELETE, UPDATE
 ON SCHEMA :: RecursosHumanos
-TO RRHH
+TO visualizadorReportes
 GO
 
--- Asignación de permisos a sinónimo para gestor de funciones.
+-- Asignación de permisos a sinónimos para gestor de funciones.
 GRANT SELECT ON Funciones.EmpleadoParaGestor TO gestorFunciones
+GO
+
+DENY INSERT, DELETE, UPDATE ON Funciones.EmpleadoParaGestor TO gestorFunciones
 GO
 
 GRANT SELECT ON Funciones.HorarioEmpleadoParaGestor TO gestorFunciones
 GO
 
-DENY SELECT ON Funciones.EmpleadoParaGestor TO RRHH
+DENY INSERT, DELETE, UPDATE ON Funciones.HorarioEmpleadoParaGestor TO gestorFunciones
 GO
 
-DENY SELECT ON Funciones.EmpleadoParaGestor to visualizadorReportes
+-- Asignación de permisos a sinónimos para visualizador de reportes.
+GRANT SELECT ON Reportes.EmpleadoParaReportes TO visualizadorReportes
 GO
 
-DENY SELECT ON Funciones.HorarioEmpleadoParaGestor to visualizadorReportes
+DENY INSERT, DELETE, UPDATE ON Reportes.EmpleadoParaReportes TO visualizadorReportes
 GO
 
-DENY SELECT ON Funciones.HorarioEmpleadoParaGestor TO RRHH
+GRANT SELECT ON Reportes.FuncionesParaReportes TO visualizadorReportes
 GO
 
+DENY INSERT, DELETE, UPDATE ON Reportes.FuncionesParaReportes TO visualizadorReportes
+GO
+
+GRANT SELECT ON Reportes.PeliculaParaReportes TO visualizadorReportes
+GO
+
+DENY INSERT, DELETE, UPDATE ON Reportes.PeliculaParaReportes TO visualizadorReportes
+GO
+
+GRANT SELECT ON Reportes.SalasParaReportes TO visualizadorReportes
+GO
+
+DENY INSERT, DELETE, UPDATE ON Reportes.SalasParaReportes TO visualizadorReportes
+GO
+
+-- Asignación de permisos para ejecutar stored procedures a gestor sobre su esquema, 
+-- configuración para evitar que otros usuarios ejecuten SPs que no sean de su esquema.
+GRANT EXECUTE 
+ON SCHEMA :: Funciones
+TO gestorFunciones
+GO
+
+DENY EXECUTE
+ON SCHEMA :: RecursosHumanos
+TO RRHH
+GO
+
+DENY EXECUTE
+ON SCHEMA :: RecursosHumanos
+TO visualizadorReportes
+GO
+
+-- Asignación de permisos para ejecutar stored procedures a recursos humanos sobre su esquema, 
+-- configuración para evitar que otros usuarios ejecuten SPs que no sean de su esquema.
+GRANT EXECUTE
+ON SCHEMA :: RecursosHumanos
+TO RRHH
+GO
+
+DENY EXECUTE
+ON SCHEMA :: RecursosHumanos
+TO gestorFunciones
+GO
+
+DENY EXECUTE
+ON SCHEMA :: RecursosHumanos
+TO visualizadorReportes
+
+-- Asignación de permisos para ejecutar stored procedures a recursos humanos sobre su esquema, 
+-- configuración para evitar que otros usuarios ejecuten SPs que no sean de su esquema.
+GRANT EXECUTE
+ON SCHEMA :: Reportes
+TO visualizadorReportes
+
+DENY EXECUTE
+ON SCHEMA :: Reportes
+TO RRHH
+
+GRANT EXECUTE
+ON SCHEMA :: Reportes
+TO gestorFunciones
--- 5.sql
+++ 5.sql
@@ -1,6 +1,6 @@
 --REPORTES
 
-USE Flicks4U
+USE Flicks4u
 GO
 
 --Reporte de funciones en una fecha especifica
@@ -11,29 +11,31 @@ BEGIN
 	SELECT FP.Nombre AS 'Pelicula', FF.SalaId, FF.FechaInicio, FF.FechaFin,
 		CONCAT(EL.Nombres, ' ', EL.Apellidos) AS 'Empleado Limpieza', 
 		CONCAT(EP.Nombres, ' ', EP.Apellidos) AS 'Empleado Proyeccion' 
-	FROM Funciones.Funcion AS FF
-	JOIN Funciones.Pelicula AS FP ON FP.Id = FF.PeliculaId
-	JOIN RecursosHumanos.Empleado AS EL ON EL.Id = FF.EmpleadoLimpiezaId
-	JOIN RecursosHumanos.Empleado AS EP ON EP.Id = FF.EmpleadoProyecionId
-	WHERE CAST(FF.FechaInicio AS DATE) = @Fecha
+	FROM Reportes.FuncionesParaReportes AS FF
+	JOIN Reportes.PeliculaParaReportes AS FP ON FP.Id = FF.PeliculaId
+	JOIN Reportes.EmpleadoParaReportes AS EL ON EL.Id = FF.EmpleadoLimpiezaId
+	JOIN Reportes.EmpleadoParaReportes AS EP ON EP.Id = FF.EmpleadoProyecionId
+	WHERE CAST(FF.FechaInicio AS DATE) = '2022-06-19'
 END
-
---EXEC FuncionesDia_SP '2022-06-19'
-
+GO
+--EXEC Reportes.Funciones_Dia_SP '2022-06-19'
 --Estadistica de funciones por genero de pelicula.
 --	--Genero, Cantidad En cartelera, Cantidad Fuera de cartelera, Total Tiempo en Pantalla
-
 --Horario de un empleado en una fecha especifica
+
 CREATE PROC Reportes.Horario_Empleado_SP
 (@EmpleadoId INT, @Fecha DATE)
 AS
 BEGIN
 	SELECT FP.Nombre AS 'Pelicula', FF.SalaId, CAST(FF.FechaInicio AS TIME(0)) AS 'Hora Inicio',
-		CAST(FF.FechaFin AS TIME(0)) AS 'Hora Fin' FROM Funciones.Funcion AS FF
-	JOIN Funciones.Pelicula AS FP ON FP.Id = FF.PeliculaId
+	CAST(FF.FechaFin AS TIME(0)) AS 'Hora Fin'
+	FROM Reportes.FuncionesParaReportes AS FF
+	JOIN Reportes.PeliculaParaReportes AS FP ON FP.Id = FF.PeliculaId
 	WHERE (EmpleadoLimpiezaId = @EmpleadoId OR EmpleadoProyecionId = @EmpleadoId)
 		AND CAST(FF.FechaInicio AS DATE) = @Fecha
 END
+GO
+-- EXEC Reportes.Horario_Empleado_SP 1, '2022-06-19'
 
 -- Permite recuperar las estadísticas de una película en base a su género.
 CREATE PROC Reportes.Estadisticas_Pelicula_Genero_SP
@@ -43,11 +45,13 @@ SELECT DISTINCT p.Nombre, p.Duracion as 'Duración', COUNT(f.PeliculaId) as 'Sal
 SUM(DATEDIFF(MINUTE,f.FechaInicio, f.FechaFin) - 35) as 'Tiempo reproducida',
 COUNT(f.Fechainicio) * 35 as 'Tiempo muerto (limpieza, avances)',
 SUM(DATEDIFF(MINUTE,f.FechaInicio, f.FechaFin)) AS 'Tiempo total acumulado'
-FROM Funciones.Pelicula as p
-inner join Funciones.Funcion as f on f.PeliculaId = p.Id
-inner join Funciones.Sala as s on s.Id = f.SalaId
-WHERE p.Genero = 'Comedia'
+FROM Reportes.PeliculaParaReportes as p
+inner join Reportes.FuncionesParaReportes as f on f.PeliculaId = p.Id
+inner join Reportes.SalasParaReportes as s on s.Id = f.SalaId
+WHERE p.Genero = @Genero
 GROUP BY p.Nombre, p.Duracion, p.Genero, f.SalaId
 GO
 
---EXEC HorarioEmpleado_SP 1, '2022-06-21'
+-- EXEC Reportes.Estadisticas_Pelicula_Genero_SP 'Comedia'
+
+
